27f6c07b4dcbca6fe8b221428331f01e2eebdcd8
Talgat Shakidaev <talgat.shakidaev0312@gmail.com>
2023-03-27 05:51:10 +0900

    Add constants for contract

--- programs/xToken-Stake/src/constant.rs
+++ programs/xToken-Stake/src/constant.rs
@@ -6,3 +6,5 @@ pub const MAX_MINT_LIMIT: usize = 300;
 pub const VAULT_REWARD_SEED: &str = "x_token_vault_reward";
 pub const VAULT_USER_SEED: &str = "x_token_vault_user";
 pub const VAULT_STAKE_SEED: &str = "x_token_vault_stake";
+pub const VAULT_CODE_SEED: &str = "x_token_value_code_seed";
+pub const VAULT_TOKEN_OFFER: &str = "x_token_count_seed";
